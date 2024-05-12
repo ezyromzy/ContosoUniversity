9bb224f1e99ea857dbca815e38d72ce36cd981c8
ezyromzy <roma.kocnev12345@gmail.com>
2024-05-12 17:04:13 +0300

    #6 Update Edit.cshtml.cs

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -5,8 +5,7 @@
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
-        public DateTime EnrollmentDate { get; set; }
-        public string Secret { get; set; }
+        public DateTime EnrollmentDate { get; set; }        
 
         public ICollection<Enrollment> Enrollments { get; set; }
     }
